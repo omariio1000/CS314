fc2b75d49d025da7e4f07b7da133ccff1b23dc80
Omar Nassar <omariio1000@gmail.com>
2023-10-05 10:02:36 -0700

    wrote report class

--- Report.py
+++ Report.py
@@ -14,9 +14,26 @@ class Report:
         self.totalConsultations = 0
         self.totalFee = 0.0
     
+    #add a record to the list
     def addRecord(self, inRecord: Record) -> bool:
         if (self.provider != inRecord.provider):
             print("Not your appointment")
             return False
         
-    
+        if (self.totalConsultations >= 999):
+            print("Too many consultations for the week!")
+            return False
+        
+        if (self.totalFee + inRecord.bill >= 99999.99):
+            print("Too much income this week!")
+            return False
+
+        if (self.recordList == None):
+            self.recordList = [inRecord]
+        else:
+            self.recordList.append(inRecord)
+        
+        self.totalConsultations += 1
+        self.totalFee += inRecord.bill
+
+        return True
