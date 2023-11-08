d0978ce8dadfc30747040ee96425ba4c6c89c2e1
Omar Nassar <omariio1000@gmail.com>
2023-11-08 15:50:17 -0800

    added some comments

--- Main.py
+++ Main.py
@@ -4,7 +4,17 @@ import Service
 import Record
 
 def main():
-    
+    records = dict()
+    services = dict()
+    members = dict()
+    providers = dict()
+
+    #FILE HANDLING STUFF FIRST
+    #CHECK RECORD DIRECTORY FOR RECORDS
+    #CHECK SERVICE DIRECTORY FOR SERVICES
+    #CHECK MEMBER DIRECTORY FOR MEMBER
+    #CHECK PROVIDER DIRECTORY FOR PROVIDERS
+
     return
 
 if __name__ == "__main__":
